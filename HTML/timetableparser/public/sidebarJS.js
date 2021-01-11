4afb7a7e154af0743fa292c73fe4cda102604dba
Abdalrhman Yasser <abdalrhman.yasser6e@gmail.com>
2021-01-11 20:13:57 +0400

    bug fixes stable version v2.0.1

--- HTML/timetableparser/public/sidebarJS.js
+++ HTML/timetableparser/public/sidebarJS.js
@@ -2,12 +2,10 @@ var mini = true;
 
 function toggleSidebar() {
   if (mini) {
-    console.log("opening sidebar");
     document.getElementById("mySidebar").style.width = "340px";
     document.getElementById("main").style.marginLeft = "340px";
     this.mini = false;
   } else {
-    console.log("closing sidebar");
     document.getElementById("mySidebar").style.width = "65px";
     document.getElementById("main").style.marginLeft = "65px";
     this.mini = true;
