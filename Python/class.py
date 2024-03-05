1bf2566cbd6295b1494fb64e4ba302e44b905d55
Abdalrhman Yaser <abdalrhman.yasser6e@gmail.com>
2024-03-05 16:04:57 +0400

    uni and other stuff. plus work on python email_thingy which got deleted

---
+++ Python/class.py
@@ -0,0 +1,18 @@
+class School:
+    def __init__(self) -> None:
+        pass
+    def enroll(S):
+        pass
+school = School()
+class Student:
+    def __init__(self, _d, name) -> None:
+        self.age = _d
+        self.name = name
+        school.enroll()
+        pass
+    def __init__(self, name) -> None:
+        self.name = name
+        pass
+
+print("hello")
+data = Student(42, "name")
